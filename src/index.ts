fb4af35485a4262809405e5775d51fc8fc578ccc
shonore <dyx769@mocs.utc.edu>
2018-07-01 21:16:51 -0400

    set up basic express app. Should be able to run with npm run on port 4001

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// src/index.ts
+import * as express from 'express'
+//import * as cors from 'cors'
+//import * as bodyParser from 'body-parser'
+//create express app
+const app = express()
+const port = 4001
+
+//app.use(bodyParser.json())
+//app.use(bodyParser.urlencoded({ extended: true }))
+//app.use(cors())
+app.get('/', (req, res, next) => {
+    res.json('Hello world')
+})
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`)
+})
