a69f2a3da42207f5fa445a2cd2b312fd788c1467
shonore <dyx769@mocs.utc.edu>
2018-07-07 13:35:10 -0400

    added faker.js file for mock data and added nodemon to watch for server changes

--- src/express.ts
+++ src/express.ts
@@ -1,9 +1,11 @@
 import * as express from 'express'
+import * as faker from 'faker'
 //import * as cors from 'cors'
 //import * as bodyParser from 'body-parser'
 //create express app
-const app = express()
-const port = 4001
+const app = express();
+//for mocking data
+var faker = require('faker');
 
 //app.use(bodyParser.json())
 //app.use(bodyParser.urlencoded({ extended: true }))
